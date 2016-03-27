61cb375b2d16f9628bf2170970fa968f61450c8b
Hassan Abbas <em6097@wayne.edu>
2016-03-26 23:27:34 -0400

    500 Bubbles

--- sketch.js
+++ sketch.js
@@ -7,10 +7,14 @@ setup = function () {
 
 draw = function () {
   background(0);
+
   bubbles.forEach(function updateAndDisplay (bubble) { // function is defined
     bubble.update();
     bubble.display();
   });
+  while (bubbles.length < 500) {
+    bubbles.push(new Bubble(random(0, 600), random(0, 600)));
+  }
   if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY)); //new prototype pushed into array
 
 };
