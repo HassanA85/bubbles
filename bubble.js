68a8f1078ae0410037add8082fb219043b64c103
Hassan Abbas <em6097@wayne.edu>
2016-03-27 01:52:54 -0400

    Bubbles move upwards continuously

--- bubble.js
+++ bubble.js
@@ -10,13 +10,15 @@ Bubble.prototype = {
   update: function() {
     this.x += random(-1, 1);
     this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    //if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
   },
 
   display: function () {
     noStroke();
     fill(102, 217, 255, 100);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    if (this.y - this.radius <= 0) this.y = 600;
+
   }
 
 };
