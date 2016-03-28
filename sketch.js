ff377dbef1e9ac42a2b7bc657d9c9c4ba324a25b
Hassan Abbas <em6097@wayne.edu>
2016-03-27 20:33:43 -0400

    Iterated Using Loop

--- sketch.js
+++ sketch.js
@@ -2,10 +2,21 @@ var bubbles = [];
 
 setup = function () {
   createCanvas(600, 600);
-  // your code goes here
+
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+  //bubbles.forEach(function updateAndDisplay (bubble) { // function is defined
+    //bubble.update();
+    //bubble.display();
+  //});
+  var index = 0;
+  while (index < bubbles.length) {
+      bubbles[index].update();
+      bubbles[index].display();
+      index += 1;
+    }; //used loop instead of callback
+  if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY)); //new prototype pushed into array
+
 };
